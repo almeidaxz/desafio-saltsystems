e31206288d4e7c405b4e1aade94c59f05b703496
almeidaxz <almeidaxz@live.com>
2022-12-07 16:41:20 -0300

    feat: criação do projeto base client

---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen bg-[#EDEDED]">
+      <aside>
+        ASIDE
+      </aside>
+      <main>
+      MAIN
+      </main>
+    </div>
+  )
+}
