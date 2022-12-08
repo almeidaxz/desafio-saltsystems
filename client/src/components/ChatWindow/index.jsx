d8a38f00ab337f18297707fa906d06ee4c439123
almeidaxz <almeidaxz@live.com>
2022-12-07 21:17:27 -0300

    feat: implementada barra e uso de emojis

--- client/src/components/ChatWindow/index.jsx
+++ client/src/components/ChatWindow/index.jsx
@@ -2,10 +2,25 @@ import SearchIcon from '@mui/icons-material/Search';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import MoodIcon from '@mui/icons-material/Mood';
 import SendIcon from '@mui/icons-material/Send';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import EmojiPicker from 'emoji-picker-react';
 
 import './styles.css';
+import { useState } from 'react';
 
 export default function ChatWindow() {
+    const [message, setMessage] = useState('');
+    const [emojiOpen, setEmojiOpen] = useState(false);
+    const previewConfig = {
+        defaultEmoji: 'none',
+        defaultCaption: 'none',
+        showPreview: false
+    }
+
+    const handleEmojiClick = (e) => {
+        setMessage(`${message}${e.emoji}`)
+    }
+
     return (
         <div className='w-full flex flex-col h-full'>
             <header
@@ -26,17 +41,44 @@ export default function ChatWindow() {
             <main className='chat-body'>
 
             </main>
+            <div
+                className='emojis-container'
+                style={{ height: emojiOpen ? '250px' : 0 }}
+            >
+                {emojiOpen &&
+                    <EmojiPicker
+                        onEmojiClick={handleEmojiClick}
+                        searchDisabled
+                        skinTonesDisabled
+                        width='full'
+                        height='inherit'
+                        previewConfig={previewConfig}
+                    />
+                }
+            </div>
             <footer
                 className='h-16 px-6 flex items-center gap-4'
             >
-                <MoodIcon
-                    className='cursor-pointer'
-                    fontSize='medium'
-                    style={{ color: '#919191' }}
-                />
+                {emojiOpen ?
+                    <CloseRoundedIcon
+                        className='cursor-pointer'
+                        fontSize='medium'
+                        style={{ color: '#919191' }}
+                        onClick={(e) => setEmojiOpen(false)}
+                    />
+                    :
+                    <MoodIcon
+                        className='cursor-pointer'
+                        fontSize='medium'
+                        style={{ color: '#919191' }}
+                        onClick={(e) => setEmojiOpen(true)}
+                    />
+                }
                 <input
                     className='flex-1 h-10 pl-4 border-none outline-none rounded-lg'
                     type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                     placeholder='Mensagem'
                 />
                 <SendIcon
