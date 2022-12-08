744a0ebf4f8450dcf566ec53fd5d5f17e87b8e7d
almeidaxz <almeidaxz@live.com>
2022-12-08 11:10:33 -0300

    fix(login): corrigido retorno e validação de dados

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,14 +1,18 @@
-const knex = require('knex');
+const knex = require('../services/dbConnection');
 
 const userSignIn = async (req, res) => {
     const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).json({ message: 'Informe todos os dados.' });
+    }
 
     try {
         const existingUser = await knex('users').where({ email }).first();
         if (!existingUser) {
             const newUser = await knex('users').insert({ name, email }).returning('*');
 
-            return res.status(201).json(newUser);
+            return res.status(201).json(newUser[0]);
         }
 
         return res.status(200).json(existingUser);
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const routes = express();
+const { userSignIn } = require('./controllers/users');
 
 routes.get('/', (req, res) => {
     return res.status(200).json({ message: "Servidor ativo" });
 });
+
+routes.post('/login', userSignIn);
 
 module.exports = routes;
